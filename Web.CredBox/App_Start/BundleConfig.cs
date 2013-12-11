acc310255c7644dabaa4d7545252812e8ad64a59
hanters <hanters@gmail.com>
2013-12-11 11:21:01 -0200

    Initial Commit

---
+++ Web.CredBox/App_Start/BundleConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Web.CredBox
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery-ui-1.10.3.custom.min.js"
+                        )
+                        
+                        );
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/Global.css",
+                "~/Content/css/jquery-ui-1.10.3.custom.min.css"
+               ));
+        }
+    }
+}
