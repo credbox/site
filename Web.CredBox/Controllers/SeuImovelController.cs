32b0cea877a8f44d4b95d1a58485e76b50bd03f9
hanters <hanters@gmail.com>
2013-12-11 18:44:20 -0200

    Add imovel, e encontre seu imovel

---
+++ Web.CredBox/Controllers/SeuImovelController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.CredBox.Controllers
+{
+    public class SeuImovelController : Controller
+    {
+        //
+        // GET: /SeuImovel/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Imovel()
+        {
+            return View();
+        }
+
+    }
+}
