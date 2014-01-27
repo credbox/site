66b514c6e93ac1d94f730d7a2508ae7bf683cf43
hanters <hanters@gmail.com>
2014-01-27 18:11:43 -0200

    opa

---
+++ Web.CredBox/Controllers/ContatoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.CredBox.Controllers
+{
+    public class ContatoController : Controller
+    {
+        //
+        // GET: /FaleConosco/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Web.CredBox/Controllers/DivulgarController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.CredBox.Controllers
+{
+    public class DivulgarController : Controller
+    {
+        //
+        // GET: /Divulgar/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
