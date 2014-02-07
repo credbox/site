b776ae6505a8b911aa95f2b3fbaf24ecdc85e410
hanters <hanters@gmail.com>
2014-02-07 19:22:19 -0200

    Add modal for login

--- Web.CredBox/App_Start/BundleConfig.cs
+++ Web.CredBox/App_Start/BundleConfig.cs
@@ -8,6 +8,7 @@ namespace Web.CredBox
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
+            BundleTable.EnableOptimizations = false;
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js",
                         "~/Scripts/jquery-ui-1.10.3.custom.min.js"
